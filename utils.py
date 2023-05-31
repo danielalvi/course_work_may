030b26ee5cf8085b4a6b216c35ba63bb6eb8db50
danielalvi <dc9204110@gmail.com>
2023-05-31 13:24:41 +0300

    create changes

--- utils.py
+++ utils.py
@@ -10,15 +10,18 @@ def get_data():
 
 
 def get_filtered_data(data, filter_empty_from=False):
-    data = [x for x in data if "state" in x["state"] == "EXECUTED"]
+
+    data = [x for x in data if "state" in x]
+    data = [x for x in data if x["state"] == "EXECUTED"]
     if filter_empty_from:
         data = [x for x in data if "from" in x]
     return data
 
 
 def get_last_values(data, count_last_values):
-    data = sorted(data, key=lambda x: x["data"], reverse=True)
+    data = sorted(data, key=lambda x: x["date"], reverse=True)
     data = data[:count_last_values]
+
     return data
 
 
@@ -28,17 +31,20 @@ def get_formated_data(data):
     for row in data:
         date = datetime.strptime(row["date"],"%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
         description = row["description"]
-        recipient = f"{row['to'].split()[0]}**{row['to'][-4:]}"
-        operation_amount = f"{row['operationAmount']['amount']}{row['operationAmount']['currency']['name']}"
+        recipient = f"{row['to'].split()[0]} ** {row['to'][-4:]}"
+
+        operation_amount = f"{row['operationAmount']['amount']} {row['operationAmount']['currency']['name']}"
         if "from" in row:
             sender = row["from"].split()
             from_bill = sender.pop(-1)
-            from_bill = f"{from_bill[:4]}{from_bill[4:6]}** **** {from_bill[-4:]}"
+            from_bill = f"{from_bill[:4]} {from_bill[4:6]} ** **** {from_bill[-4:]}"
             from_info = "".join(sender)
         else:
-            from_info,from_bill = "", ""
+            from_info, from_bill = "", ""
         formated_data.append(f"""\
-{date}{description}
-{from_info}{from_bill} -> {recipient}
+{date} {description}
+{from_info} {from_bill} -> {recipient}
 {operation_amount}""")
     return formated_data
+
+
